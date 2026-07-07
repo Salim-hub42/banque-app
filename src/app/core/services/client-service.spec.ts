064e195cd25a1d0f3aac7274cdfa41a3856f64c0
salim-hub42 <salim.khalfoun@yahoo.fr>
2026-07-07 16:01:42 +0200

    face de testing

--- src/app/core/services/client-service.spec.ts
+++ src/app/core/services/client-service.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ClientService } from './client-service';
+import { provideHttpClient   } from '@angular/common/http';
+import { provideHttpClientTesting, HttpTestingController } from '@angular/common/http/testing';
+import { MessageService } from 'primeng/api';
+
 
 describe('ClientService', () => {
   let service: ClientService;
+  let httpMock: HttpTestingController;
+  let message: MessageService;
+
+ 
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting(), MessageService]
+    });
     service = TestBed.inject(ClientService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('charge les clients depuis le serveur', () => {
+    service.chargerClients();
+
+    const requete = httpMock.expectOne('http://localhost:3000/clients');
+    expect(requete.request.method).toBe('GET');
+
+    requete.flush([{id: '1', nom: 'Dupont', prenom: 'Jean', email: '', telephone: '', dateNaissance: '', adresse: ''}]);
+    
+      expect(service.clients()).toHaveLength(1);
+      expect(service.clients()[0].nom).toBe('Dupont');
   });
+  afterEach(() => httpMock.verify())
 });
--- src/app/shared/validators/iban-validator/iban-validator.spec.ts
+++ src/app/shared/validators/iban-validator/iban-validator.spec.ts
@@ -1,22 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormControl } from '@angular/forms';
 
-import { IbanValidator } from './iban-validator';
+import { ibanFrancaisValidator } from './iban-validator';
 
-describe('IbanValidator', () => {
-  let component: IbanValidator;
-  let fixture: ComponentFixture<IbanValidator>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [IbanValidator],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(IbanValidator);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
+describe('ibanFrancaisValidator', () => {
+  it('accepte une valeur vide (le champ optionnel est valide)', () => {
+    const control = new FormControl('');
+    expect(ibanFrancaisValidator(control)).toBeNull();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('accepte un IBAN français valide', () => {
+    const control = new FormControl('FR1420041010050500013M02606');
+    expect(ibanFrancaisValidator(control)).toBeNull();
+  });
+
+  it('accepte un IBAN français valide avec des espaces', () => {
+    const control = new FormControl('FR14 2004 1010 0505 0001 3M02 606');
+    expect(ibanFrancaisValidator(control)).toBeNull();
+  });
+
+  it('rejette un IBAN qui ne commence pas par FR', () => {
+    const control = new FormControl('DE1420041010050500013M02606');
+    expect(ibanFrancaisValidator(control)).toEqual({ ibanInvalide: true });
+  });
+
+  it('rejette un IBAN trop court', () => {
+    const control = new FormControl('FR1420041010');
+    expect(ibanFrancaisValidator(control)).toEqual({ ibanInvalide: true });
   });
 });
